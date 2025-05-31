152cd9b3c3a30055a760c57192a215ec97d524c4
Riley Tran <s4092817@student.rmit.edu.au>
2025-05-31 16:34:47 +1000

    tested + fully implemented compareFiles() for testing purposes

--- assignment_four/src/test/java/com/example/AppTest.java
+++ assignment_four/src/test/java/com/example/AppTest.java
@@ -1,13 +1,76 @@
 package com.example;
 
 import static org.junit.Assert.assertTrue;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    // THE FOLLOWING FUNCTION IS WHITESPACE SENSITIVE
+    public boolean compareFiles(String file1, String file2) {
+
+        // Initialize readers for the two files
+
+        String line;
+
+        ArrayList<String> lines1 = new ArrayList<>();
+        ArrayList<String> lines2 = new ArrayList<>();
+
+
+        try {
+            BufferedReader reader1 = new BufferedReader(new FileReader("src/output/"+file1));
+            while ((line = reader1.readLine()) != null) {
+                lines1.add(line);
+            }
+            reader1.close();
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            return false;
+
+        }
+
+        try {
+            BufferedReader reader2 = new BufferedReader(new FileReader("src/output/"+file2));
+            while ((line = reader2.readLine()) != null) {
+                lines2.add(line);
+            }
+            reader2.close();
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            return false;
+        }
+        
+
+    // Compare sizes first
+    System.out.println("Lines in file 1: " + lines1.size());
+    System.out.println("Lines in file 2: " + lines2.size());
+
+    if (lines1.size() != lines2.size()) {
+        return false;
+    }
+
+    // Compare contents line-by-line
+    for (int i = 0; i < lines1.size(); i++) {
+        if (!lines1.get(i).equals(lines2.get(i))) {
+            return false;
+        }
+    }
+        
+
+        return true;
+    }
+
     /**
      * Rigorous Test :-)
      */
@@ -16,5 +79,13 @@ public class AppTest
     {
         assertTrue( true );
     }
+    @Test
+    public void testFiles() {
+        String expectedFile = "functTest1.txt";
+        String actualFile = "functTest2.txt";
+
+        assertTrue(compareFiles(expectedFile, actualFile));
+    }
+
     
 }
